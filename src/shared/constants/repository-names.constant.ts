122e01e67cccd4aa4baae78206be890329e6a46d
apotheddy <apotheddy@hotmail.com>
2026-04-03 21:50:40 -0500

    feat(repository-names): agregar nueva variable para el repositorio de correo [as-47]

--- src/shared/constants/repository-names.constant.ts
+++ src/shared/constants/repository-names.constant.ts
@@ -1,3 +1,4 @@
 export class RepositoryNames {
   public static readonly ACCOUNT_REPOSITORY: string = 'ACCOUNT_REPOSITORY';
+  public static readonly MAILER_REPOSITORY: string = "MAILER_REPOSITORY";
 }
